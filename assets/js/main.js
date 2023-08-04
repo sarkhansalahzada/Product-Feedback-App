6bed7f2d551f7e6964c6e64f4dbb230f46a4083f
sarkhansalahzada <ssalahzada@gmail.com>
2023-08-04 18:57:21 +0400

    uptaded js

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,19 +13,3 @@ const menuToggle = document.getElementById('menu__toggle');
       mainBar.style.display = 'none';
     }
   });
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const sortDropdown = document.getElementById("sort");
-//   sortDropdown.value = "most-up";
-// });
-
-// const menuToggle = document.getElementById('menu__toggle');
-// const mainBar = document.querySelector('.main-bar');
-
-// menuToggle.addEventListener('change', () => {
-//   if (menuToggle.checked) {
-//     mainBar.style.right = '0'; // Slide in from the right
-//   } else {
-//     mainBar.style.right = '-250px'; // Slide out to the right
-//   }
-// });
