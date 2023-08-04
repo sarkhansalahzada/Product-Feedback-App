7f9ed4e837525d8e1cbddafcc18a32fb7292de3d
sarkhansalahzada <ssalahzada@gmail.com>
2023-08-04 15:22:16 +0400

    added-new-features

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,30 @@ document.addEventListener("DOMContentLoaded", function () {
   const sortDropdown = document.getElementById("sort");
   sortDropdown.value = "most-up";
 });
+
+const menuToggle = document.getElementById('menu__toggle');
+  const mainBar = document.querySelector('.main-bar');
+
+  menuToggle.addEventListener('change', () => {
+    if (menuToggle.checked) {
+      mainBar.style.display = 'block';
+    } else {
+      mainBar.style.display = 'none';
+    }
+  });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const sortDropdown = document.getElementById("sort");
+//   sortDropdown.value = "most-up";
+// });
+
+// const menuToggle = document.getElementById('menu__toggle');
+// const mainBar = document.querySelector('.main-bar');
+
+// menuToggle.addEventListener('change', () => {
+//   if (menuToggle.checked) {
+//     mainBar.style.right = '0'; // Slide in from the right
+//   } else {
+//     mainBar.style.right = '-250px'; // Slide out to the right
+//   }
+// });
